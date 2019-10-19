e2c44313eb1aae5c573e3543a98fcfc8f6f0f749
Chloe Kuo <42684149+ckuo23@users.noreply.github.com>
2019-10-19 05:09:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,37 @@
+
+import time_mode as time_mode
+import light_up as light_up
+
 from time_mode import *
+from light_up import *
+changed = 0;
 
 def main():
-    setValidMatrix(getHour(),getMinutes())
-    print(np.matrix(Matrix))
+    light_up.clear_LEDs()
+    while 1:
+        if (time_mode.getMinutes() == 0 and time_mode.getSeconds() == 0):
+            light_up.disp_clear_hour()
+            time_mode.set_hour(time_mode.getHour())
+            time_mode.set_minutes(time_mode.getMinutes())
+            time_mode.set_base_words()
+            light_up.disp_time()
+            time.sleep(1)
+            
+        elif (time_mode.getMinutes() % 5 == 0 and time_mode.getSeconds() == 0):
+            light_up.disp_clear_mins()
+            time_mode.set_hour(time_mode.getHour())
+            time_mode.set_minutes(time_mode.getMinutes())
+            time_mode.set_base_words()
+            light_up.disp_time()
+            time_mode.clear_matrix()
+            time.sleep(1)
+
+        else:
+            time_mode.set_hour(time_mode.getHour())
+            time_mode.set_minutes(time_mode.getMinutes())
+            time_mode.set_base_words()
+            light_up.disp_time()
+            time_mode.clear_matrix()
 main()
+
+
