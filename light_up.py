a415ee079fa0c62c544f81cf40fd03119a5d70c1
Chloe Kuo <42684149+ckuo23@users.noreply.github.com>
2019-11-03 17:53:54 -0500

    Update light_up.py

--- light_up.py
+++ light_up.py
@@ -10,6 +10,9 @@ from mapping_matrix import *
 
 # Uses mapping matrix to assign which LEDs to light up and lights up LEDs
 def disp_time():
+    time_mode.set_base_words()
+    time_mode.set_hour(time_mode.getHour())
+    time_mode.set_minutes(time_mode.getMinutes())
     for j in range(12):
         for k in range(12):
             if (time_mode.Matrix[k][j] == 1):
@@ -17,24 +20,16 @@ def disp_time():
                 pixels[int(m[k][j])] = (0, 255, 0)
 
 # clears LEDs except for words "it" "is" and "oclock"
-def disp_clear_hour():
-    time_mode.clear_hour()
-    time_mode.clear_mins()
+def disp_clear_middle():
+    time_mode.clear_middle()
     for j in range(12):
-        for k in range(5):
+        for k in range(8):
             m = mapping_matrix.getMatrix() # create mapping matrix
-            pixels[int(m[k+4][j])] = (0, 0, 0)
-            
-# clears LEDs except for words "it" "is" and "oclock" and "hour"
-def disp_clear_mins():
-    time_mode.clear_mins()
-    for j in range(12):
-        for k in range(3):
-            m = mapping_matrix.getMatrix() # create mapping matrix
-            if (k+1 < 4):
+            if k == 8 and j <= 3:
+                pixels[int(m[9][j])] = (0, 0, 0)
+            elif k != 8:
                 pixels[int(m[k+1][j])] = (0, 0, 0)
-            elif (k+1 == 4 and j <= 7):
-                pixels[int(m[k+1][j])] = (0, 0, 0)
+
 
 def clear_LEDs():
     pixels.fill((0,0,0))
