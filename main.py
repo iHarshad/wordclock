0bc069f35517513d284eb5c312c825d72add9261
Chloe Kuo <42684149+ckuo23@users.noreply.github.com>
2019-11-11 16:24:35 -0500

    added switching modes between clock and snake

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-
 import time_mode as time_mode
 import light_up as light_up
-
+import snake_mode as snake_mode
+from snake_mode import *
 from time_mode import *
 from light_up import *
 changed = 0;
 
-def main():
+def clock():
     light_up.clear_LEDs()
     light_up.disp_time()
     #print(time_mode.getMinutes())
@@ -19,7 +19,15 @@ def main():
                 light_up.disp_time()
                 time.sleep(1)
         
+def snake():
+    snake_mode.button_init()
+    snake_mode.generate_apple()
+    while snake_mode.end == False:
+        snake_mode.disp_snake()
+        snake_mode.move_snake()
+        time.sleep(0.1)
+    snake_mode.end_game()
 
-main()
+snake()
 
 
