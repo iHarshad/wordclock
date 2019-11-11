e885c36d3dea87ceecc8683a8ee7a9698ce82507
Chloe Kuo <42684149+ckuo23@users.noreply.github.com>
2019-11-11 03:20:32 -0500

    Update snake_mode.py

--- snake_mode.py
+++ snake_mode.py
@@ -1,6 +1,7 @@
 import numpy
 import board
 import neopixel
+import random
 pixels = neopixel.NeoPixel(board.D18, 144,  brightness=0.2, auto_write=False)
 
 import mapping_matrix as mapping_matrix
@@ -12,6 +13,7 @@ import time
 Matrix = np.zeros((12,12))
 snake = [[5,5], [5,4], [5,3],[5,2]]
 apple = [0,0]
+end = False
 
 def button_init():
     GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)#Button to GPIO23
@@ -32,6 +34,12 @@ def disp_snake():
         else:
             pixels[int(m[snake_x][snake_y])] = (0, 255, 0)
     pixels.show()
+    for i in range(12):
+        for j in range(12):
+            if Matrix[i][j] == 2:
+                pixels[int(m[i][j])] = (255, 0, 0)
+    pixels.show()
+
 
 def generate_apple():
     global apple
@@ -42,10 +50,7 @@ def generate_apple():
         y = random.randint(0,11)
     apple = [x,y]
     Matrix[x][y] = 2
-    m = mapping_matrix.getMatrix()
-    pixels[int(m[x][y])] = (255, 0, 0)
-    pixels.show()
-
+    
 def get_x(arr):
     return arr[0]
 
@@ -53,24 +58,24 @@ def get_y(arr):
     return arr[1]
 
 def get_button_press():
-    top = GPIO.input(23)
-    bottom = GPIO.input(17)
-    right = GPIO.input(27)
-    left = GPIO.input(22)
+    while True:
+        top = GPIO.input(23)
+        bottom = GPIO.input(17)
+        right = GPIO.input(27)
+        left = GPIO.input(22)
 
-    if top == False:
-        return 0;
-    elif bottom == False:
-        return 1;
-    elif right == False:
-        return 2;
-    elif left == False:
-        return 3;
+        if top == False:
+            return 0;
+        elif bottom == False:
+            return 1;
+        elif right == False:
+            return 2;
+        elif left == False:
+            return 3;
 
 def move_snake():
-    x = snake.get_x(snake[0])
-    y = snake.get_y(snake[0])
-    snake.insert(0, [x,y])
+    x = get_x(snake[0])
+    y = get_y(snake[0])
 
     direction = get_button_press()
     boundary = True
@@ -88,12 +93,12 @@ def move_snake():
             boundary = False
             location = [x, y+1]
     elif direction == 3:
-        if y-1 <= 11: # boundary condition
+        if y-1 >= 0: # boundary condition
             boundary = False
             location = [x, y-1]
 
     if boundary == False:
-        snake[0] = location
+        snake.insert(0, location)
         check_snake_hit(location)
         if location != apple:
             snake.pop(len(snake)-1)
@@ -101,22 +106,26 @@ def move_snake():
             generate_apple()
 
 def check_snake_hit(location):
+    global end
     if snake.count(location) > 1:
-        end_game()
+        end = True
 
 def end_game():
     # flash lights
     while True:
-        disp_snake()
-        time.sleep(0.5)
         pixels.fill((0,0,0))
-        time.sleep(0.5)
+        pixels.show()
+        time.sleep(1)
+        disp_snake()
+        time.sleep(1)
 
 def main():
+    global end
+    button_init()
     generate_apple()
-
-    while True:
+    while end == False:
         disp_snake()
         move_snake()
         time.sleep(0.2)
+    end_game()
 main()
