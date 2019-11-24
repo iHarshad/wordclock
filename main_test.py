0cc346d1ad3305f5c33106fe3cb65c5ee0e2d4d0
Chloe Kuo <42684149+ckuo23@users.noreply.github.com>
2019-11-24 13:16:52 -0500

    added switch mode capability

--- main_test.py
+++ main_test.py
@@ -15,40 +15,50 @@ def clock(): #mode 0
     #print(time_mode.getHour())
     
     while (1):
-        if time_mode.getSeconds() == 0:
-            if time_mode.getMinutes() % 5 == 0:
-                light_up.disp_clear_middle()
-                light_up.disp_time()
-                time.sleep(1)
-        check_mode_change(); # may effect the check on every five mins since delay incurred
-        
+        if mode == 0:
+            if time_mode.getSeconds() == 0:
+                if time_mode.getMinutes() % 5 == 0:
+                    light_up.disp_clear_middle()
+                    light_up.disp_time()
+                    time.sleep(1)
+            check_mode_change(); # may effect the check on every five mins since delay incurred
+        else:
+            snake_mode.reset_game()
+            snake();
+
 def snake():
+    global mode
     snake_mode.button_init()
     snake_mode.generate_apple()
     while snake_mode.end == False:
-        snake_mode.disp_snake()
-        snake_mode.move_snake()
-        time.sleep(0.1)
-        check_mode_change();
-    snake_mode.end_game() # need to check for mode change here
+        if mode == 1:
+            snake_mode.disp_snake()
+            snake_mode.move_snake()
+            check_mode_change()
+            time.sleep(0.1)
+            #check_mode_change()
+        else:
+            clock();
+    snake_mode.end = False;
+    while True:
+        if mode == 1:
+            snake_mode.end_game()
+            check_mode_change()
+        else:
+            clock();
 
 def check_mode_change():
-    timeout = .1
-    timeout_start = time.time()
+    global mode
+    pressed = GPIO.input(25)
+    if pressed == False:
+        mode = 1;
+    else:
+        mode = 0;
     
-    while time.time() < timeout_start + timeout:
-        pressed = GPIO.input(25)
-        if pressed == False:
-            if mode == 0:
-                #clear matrix?
-                snake();
-            elif mode == 1:
-                #clear matrix and reset snake?
-                clock();
-
 def main():
     GPIO.setup(25, GPIO.IN, pull_up_down=GPIO.PUD_UP)#setup button, change XXX to actual gpio pin number
     clock()
 
 
-clock()
+main()
+
